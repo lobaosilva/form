e8aa9aaf94bd8d47a1555e2913b998baebbd16bb
Lobão Silva <lobaocode@outlook.com>
2023-05-28 14:55:10 -0300

    Subindo teste form

---
+++ form-script.js
@@ -0,0 +1,28 @@
+function sendEmail() {
+    let firstname = document.getElementById("firstname").value;
+    let lastname = document.getElementById("lastname").value;
+    let email = document.getElementById("email").value;
+    let phone = document.getElementById("phone").value;
+    let jobtitle = document.getElementById("jobtitle").value;
+    let jobgame = document.getElementById("jobgame").value;
+    let message = document.getElementById("message").value;
+
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "user-email@gmail.com",
+        Password: "password-email",
+        To: "evandeilsonjose@gmail.com",
+        From: email,
+        Subject: jobtitle,
+        Body: "Nome: " + firstname + " " + lastname + 
+              "<br>Email: " + email + " Telefone: " + phone + 
+              "<br>Interesse: " + jobtitle + " Simulador: " + jobgame + 
+              "<br>Mensagem: " + message
+    }).then(function (response) {
+        if (response === "OK") {
+            alert("Email enviado com sucesso.");
+        } else {
+            throw new Error("Erro ao enviar email.");
+        }
+    });
+}
